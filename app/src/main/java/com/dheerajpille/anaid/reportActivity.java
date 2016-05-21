ef6e80aefd59b1cca5adc129c3b38d4709332236
Dheeraj Pille <dheerajpille@gmail.com>
2016-05-20 21:26:18 -0400

    Changed camera capture method for reprot feature.

--- app/src/main/java/com/dheerajpille/anaid/reportActivity.java
+++ app/src/main/java/com/dheerajpille/anaid/reportActivity.java
@@ -2,6 +2,7 @@ package com.dheerajpille.anaid;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
@@ -10,13 +11,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.File;
 
 public class reportActivity extends AppCompatActivity {
 
     Button button;
     ImageView imageView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,24 +29,18 @@ public class reportActivity extends AppCompatActivity {
     }
 
     public void onClickCapture(View v) {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File file = getFile();
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-        startActivity(cameraIntent);
-    }
-
-    private File getFile() {
-        File folder = new File("sdcard/camera_app");
-        if (!folder.exists()){
-            folder.mkdir();
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-        File image_file = new File(folder,"cam_image.jpg");
-        return image_file;
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String path = "sdcard/camera_app/cam_image.jpg";
-        imageView.setImageDrawable(Drawable.createFromPath(path));
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
